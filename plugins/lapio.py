032cbd8fe5746d02ea7ab4c53127e6fa84c1ce75
Antti Jaakkola <antti.jaakkola@remod.fi>
2015-11-30 16:47:17 +0200

    lapio plugin

---
+++ plugins/lapio.py
@@ -0,0 +1,42 @@
+# Lapio module. Transfer messages from channel 1 to channel 2
+
+from plugins.honkplugin import HonkPlugin
+import time
+from datetime import date
+
+
+class Lapio(HonkPlugin):
+    __name__ = "lapio"
+    __target__ = "#log"
+
+    def _plugin_init(self):
+        print("Lapio started")
+        if not self._irc_client.join_channel(self.__target__):
+            print("Join failed :/")
+            return
+        print("Joined to channel %s" % self.__target__)
+        self._irc_client.send_message("Lapio started", self.__target__)
+        self._irc_client.send_message("Lapio started, logging to %s" % self.__target__)
+        
+
+    def process_incoming(self, message):
+        print("%s" % (message,))
+        if message['action'] != 'PRIVMSG':
+            return
+        if message['target'] == self.__target__:
+            return
+        who = message['source'].split('@')[0].split("!")[0]
+
+        if '#' not in message['data']:
+            return
+        # #HASHTAG!!!!
+
+        msg = message['data']
+
+        year, week, weekday = date.today().isocalendar()
+
+        msg = "%s #year%s #week%s" % (msg, year, week)
+
+        print("Logging message: %s" % msg)
+
+        self._irc_client.send_message("%s: %s" % (who, msg), channel=self.__target__)
